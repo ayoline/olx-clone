a9ad30e47225fed9293997113cbc37a473342204
ayoline <ayo@outlook.com.br>
2021-11-13 12:43:49 -0300

    Começa a listar os anúncios

--- lib/views/Anuncios.dart
+++ lib/views/Anuncios.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: file_names
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:olx_clone/models/Anuncio.dart';
 import 'package:olx_clone/util/Configuracoes.dart';
+import 'package:olx_clone/views/widgets/ItemAnuncio.dart';
 
 class Anuncios extends StatefulWidget {
   const Anuncios({Key? key}) : super(key: key);
@@ -14,6 +18,8 @@ class _AnunciosState extends State<Anuncios> {
   List<String> itensMenu = [];
   List<DropdownMenuItem<String>> _listaItensDropEstados = List.empty();
   List<DropdownMenuItem<String>> _listaItensDropCategorias = List.empty();
+
+  final _controller = StreamController<QuerySnapshot>.broadcast();
   String? _itemSelecionadoEstado;
   String? _itemSelecionadoCategoria;
 
@@ -23,6 +29,7 @@ class _AnunciosState extends State<Anuncios> {
 
     _carregarItensDropDown();
     _verificarUsuarioLogado();
+    _adicionarListenerAnuncios();
   }
 
   @override
@@ -95,10 +102,64 @@ class _AnunciosState extends State<Anuncios> {
                 )
               ],
             ),
+            StreamBuilder(
+              stream: _controller.stream,
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                  case ConnectionState.active:
+                  case ConnectionState.done:
+                    QuerySnapshot? querySnapshot =
+                        snapshot.data as QuerySnapshot;
+
+                    if (querySnapshot.docs.isEmpty) {
+                      return Container(
+                        padding: const EdgeInsets.all(25),
+                        child: const Text(
+                          "Nenhum anúncio! :( ",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      );
+                    }
+
+                    return Expanded(
+                      child: ListView.builder(
+                          itemCount: querySnapshot.docs.length,
+                          itemBuilder: (_, indice) {
+                            List<DocumentSnapshot> anuncios =
+                                querySnapshot.docs.toList();
+                            DocumentSnapshot documentSnapshot =
+                                anuncios[indice];
+                            Anuncio anuncio =
+                                Anuncio.fromDocumentSnapshot(documentSnapshot);
+
+                            return ItemAnuncio(
+                              anuncio: anuncio,
+                              onTapItem: () {},
+                            );
+                          }),
+                    );
+                }
+                return Container();
+              },
+            ),
           ],
         ),
       ),
     );
+  }
+
+  Future<Stream<QuerySnapshot>?> _adicionarListenerAnuncios() async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    Stream<QuerySnapshot> stream = db.collection("anuncios").snapshots();
+
+    stream.listen((dados) {
+      _controller.add(dados);
+    });
   }
 
   _carregarItensDropDown() {
